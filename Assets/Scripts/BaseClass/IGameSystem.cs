6c12dd459058c325ce2a6d6cf3309dfc11b5bc75
wang <wang>
2019-04-13 14:03:52 +0800

    --- build systems

---
+++ Assets/Scripts/BaseClass/IGameSystem.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public abstract class IGameSystem {
+
+    public virtual void OnInit() { }
+    public virtual void OnUpdate() { }
+    public virtual void OnExit() { }
+
+}
---
+++ Assets/Scripts/BaseClass/IUISystem.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+class IUISystem {
+
+    public virtual void OnInit() { }
+    public virtual void OnUpdate() { }
+    public virtual void OnExit() { }
+
+}
