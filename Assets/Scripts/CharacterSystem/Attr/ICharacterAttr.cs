8cd3984337904b70bc56b69032f3d8af4954d607
wang <wang>
2019-04-13 14:52:54 +0800

    --- 角色基类的优化，将数值类属性单独出来

---
+++ Assets/Scripts/CharacterSystem/Attr/ICharacterAttr.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public class ICharacterAttr {
+
+    protected string mName;
+    protected int mMaxHP;
+    protected float mMoveSpeed;
+
+    protected int mCurrentHp;
+    protected string mIconSprite;
+}
--- Assets/Scripts/CharacterSystem/ICharacter.cs
+++ Assets/Scripts/CharacterSystem/ICharacter.cs
@@ -6,15 +6,10 @@ using UnityEngine;
 using UnityEngine.AI;
 
 public abstract class ICharacter {
-    protected string mName;
-    protected int mMaxHP;
-    protected float mMoveSpeed;
 
-    protected int mCurrentHp;
+    protected ICharacterAttr mAttr;
 
     protected GameObject mContent;
     protected NavMeshAgent mNavAgent;
     protected AudioSource mAudio;
-
-    protected string mIconSprite;
 }
