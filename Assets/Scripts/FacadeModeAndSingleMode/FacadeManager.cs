b97e3b6cc1bb9b0fdf0970478cf12f315cac4ef3
wang <wang>
2019-04-13 12:45:58 +0800

    Facade Mode And Single Mode: Single Mode Finish

---
+++ Assets/Scripts/FacadeModeAndSingleMode/FacadeManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets.Scripts.FacadeModeAndSingleMode {
+    public class FacadeManager {
+
+
+        private static FacadeManager instant;
+        public static FacadeManager Instant {
+            get {
+                if (instant == null) {
+                    instant = new FacadeManager();
+                }
+                return instant;
+            }
+        }
+
+
+
+
+
+
+
+
+    }
+}
