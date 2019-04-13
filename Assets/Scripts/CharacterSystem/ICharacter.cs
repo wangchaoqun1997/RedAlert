cc4be14337d39f5b58e468fd79e10ba8652dd351
wang <wang>
2019-04-13 14:49:03 +0800

    --- 创建角色的基类

---
+++ Assets/Scripts/CharacterSystem/ICharacter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using UnityEngine.AI;
+
+public abstract class ICharacter {
+    protected string mName;
+    protected int mMaxHP;
+    protected float mMoveSpeed;
+
+    protected int mCurrentHp;
+
+    protected GameObject mContent;
+    protected NavMeshAgent mNavAgent;
+    protected AudioSource mAudio;
+
+    protected string mIconSprite;
+}
