b3c892e1c938cfb334718586373ee57a401dc8da
12379 <wangchaoqun1997@126.com>
2019-05-19 10:43:19 +0800

    建造者模式：对于负责的对象将实例化过程分成多个组成部分，分别实现，通过Director管理所有部分的顺序

---
+++ Assets/Scripts/Factory/CharacterFactory/Builder/CharacterBuilderDirector.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public class CharacterBuilderDirector {
+    public static ICharacter Construct(ICharacterBuilder builder) {
+        builder.AddCharacterAttr();
+        builder.AddGameObject();
+        builder.AddWeapon();
+        return builder.GetResult();
+    }
+}
---
+++ Assets/Scripts/Factory/CharacterFactory/Builder/SoliderBulider.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+public class SoliderBulider : ICharacterBuilder {
+    public SoliderBulider(ICharacter character, Type t, WeaponType weaponType, Vector3 spawnPosition, int lv) : base(character, t, weaponType, spawnPosition, lv) {
+    }
+
+    public override void AddCharacterAttr() {
+        throw new NotImplementedException();
+    }
+
+    public override void AddGameObject() {
+        throw new NotImplementedException();
+    }
+
+    public override void AddInCharacterSystem() {
+        throw new NotImplementedException();
+    }
+
+    public override void AddWeapon() {
+        throw new NotImplementedException();
+    }
+
+    public override ICharacter GetResult() {
+        throw new NotImplementedException();
+    }
+}
